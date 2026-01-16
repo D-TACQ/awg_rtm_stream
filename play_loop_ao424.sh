944b108bb2b72d2b8f6717b73f1bafa939bf2381
sambelltacq <Sam.Bell@d-tacq.co.uk>
2026-01-16 17:49:47 +0000

    Play scripts now both wait for UUT ready correctly 	modified:   play_loop_ao422.sh 	modified:   play_loop_ao424.sh

--- play_loop_ao424.sh
+++ play_loop_ao424.sh
@@ -3,6 +3,22 @@
 UUT=${1:-acq1001_084}
 DELAY=${2:-1}
 
+wait_for_uut_ready() {
+	while true; do
+		STATE=$(echo rc_local_complete | nc $UUT 4220)
+		echo STATE $STATE
+		if echo $STATE | grep -q 'rclc UP acq...._... ..:..:.. up'; then
+			echo UUT is ready!
+			break
+		fi
+		sleep 1
+	done
+}
+
+wait_for_uut_ready
+
+set -e
+
 while true; do
     for file in ao424_patterns/*; do
         cat $file
